7f74972d4ca620075f27c48ce75d39f76cb2891d
Aro Arko <aroarko.sd@gmail.com>
2023-02-19 23:21:24 +0800

    firebase problem

---
+++ src/components/Nav/NavBar.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCC8thb06u4ehkQJlu53N6avLkOrGB9X94",
+  authDomain: "red-onion-52ab7.firebaseapp.com",
+  projectId: "red-onion-52ab7",
+  storageBucket: "red-onion-52ab7.appspot.com",
+  messagingSenderId: "736642012932",
+  appId: "1:736642012932:web:53c001e706f15a99cc6251"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
